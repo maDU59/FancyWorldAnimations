52f0b3897f99b9490e9e3685fcd5070db4e9c5cb
maDU59_ <matthias.ducourant@gmail.com>
2026-03-17 21:32:29 +0100

    Use of a predicate instead of multiple times the same conditions

--- src/client/java/fr/madu59/fwa/mixin/client/LevelChunkMixin.java
+++ src/client/java/fr/madu59/fwa/mixin/client/LevelChunkMixin.java
@@ -2,7 +2,7 @@ package fr.madu59.fwa.mixin.client;
 
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.Set;
+import java.util.function.Predicate;
 
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelHeightAccessor;
@@ -32,6 +32,14 @@ public abstract class LevelChunkMixin extends ChunkAccess {
     @Unique
     private final BlockState fwa$AIR_STATE = Blocks.AIR.defaultBlockState();
 
+    public static final Predicate<BlockState> IS_ANIMATED_BLOCK = state -> 
+    state.is(Blocks.END_PORTAL_FRAME) || 
+    state.is(Blocks.LECTERN) || 
+    state.is(Blocks.JUKEBOX) || 
+    state.is(Blocks.BELL) ||
+    state.getBlock() instanceof LanternBlock ||
+    state.getBlock() instanceof ChainBlock;
+
     protected LevelChunkMixin(ChunkPos chunkPos, UpgradeData upgradeData, LevelHeightAccessor levelHeightAccessor, PalettedContainerFactory palettedContainerFactory, long l, LevelChunkSection @Nullable [] levelChunkSections, @Nullable BlendingData blendingData) {
         super(chunkPos, upgradeData, levelHeightAccessor, palettedContainerFactory, l, levelChunkSections, blendingData);
     }
@@ -52,23 +60,13 @@ public abstract class LevelChunkMixin extends ChunkAccess {
 
     @Unique
     private void fwa$scanSectionFor(LevelChunkSection section, BlockPos pos, int minY){
-        if (section.getStates().maybeHas(state -> state.is(Blocks.END_PORTAL_FRAME) || 
-                                              state.is(Blocks.LECTERN) || 
-                                              state.is(Blocks.JUKEBOX) || 
-                                              state.is(Blocks.BELL)    ||
-                                              state.getBlock() instanceof LanternBlock ||
-                                              state.getBlock() instanceof ChainBlock)){
+        if (section.getStates().maybeHas(IS_ANIMATED_BLOCK)){
             for (int y = 0; y < 16; y++) {
                 for (int z = 0; z < 16; z++) {
                     for (int x = 0; x < 16; x++) {
                         BlockState state = section.getBlockState(x, y, z);
                         
-                        if (state.is(Blocks.END_PORTAL_FRAME) || 
-                                              state.is(Blocks.LECTERN) || 
-                                              state.is(Blocks.JUKEBOX) || 
-                                              state.is(Blocks.BELL)    ||
-                                              state.getBlock() instanceof LanternBlock ||
-                                              state.getBlock() instanceof ChainBlock) {
+                        if (IS_ANIMATED_BLOCK.test(state)) {
                             BlockPos worldPos = pos.offset(x, minY + y, z);
                             FancyWorldAnimationsClient.onBlockUpdate(worldPos, fwa$AIR_STATE, state);
                         }
