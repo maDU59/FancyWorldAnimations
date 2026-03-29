e065dfc1c655fdec6449ceb17d69363c21778f9e
maDU59_ <matthias.ducourant@gmail.com>
2026-03-29 15:25:00 +0200

    Fixed issues with amendments

--- src/client/java/fr/madu59/fwa/compat/ModCompat.java
+++ src/client/java/fr/madu59/fwa/compat/ModCompat.java
@@ -1,6 +1,7 @@
 package fr.madu59.fwa.compat;
 
 import fr.madu59.fwa.FancyWorldAnimationsClient.Type;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -9,6 +10,8 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 public class ModCompat {
 
     private final static String DRAMATIC_DOORS_NAMESPACE = "dramaticdoors";
+    private final static boolean IS_AMENDMENTS_LOADED = FabricLoader.getInstance().isModLoaded("amendments");
+    private final static boolean IS_IRIS_LOADED = FabricLoader.getInstance().isModLoaded("iris");
     
     public static Type typeOf(Block block){
         if(DRAMATIC_DOORS_NAMESPACE.equals(BuiltInRegistries.BLOCK.getKey(block).getNamespace())) return Type.DOOR;
@@ -18,6 +21,14 @@ public class ModCompat {
     public static boolean isOpen(BlockState state, Block block){
         if (DRAMATIC_DOORS_NAMESPACE.equals(BuiltInRegistries.BLOCK.getKey(block).getNamespace())) return state.getValue(BlockStateProperties.OPEN);
         return false;
+    }
+
+    public static boolean isAmendmentsLoaded(){
+        return IS_AMENDMENTS_LOADED;
+    }
+
+    public static boolean isIrisLoaded(){
+        return IS_IRIS_LOADED;
     }
 
 }
