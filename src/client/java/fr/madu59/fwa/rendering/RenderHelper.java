8ba931bfdcb02bb7bda88b5dcc22e68f426d0019
maDU59_ <matthias.ducourant@gmail.com>
2026-03-31 13:00:41 +0200

    Improved rendering

--- src/client/java/fr/madu59/fwa/rendering/RenderHelper.java
+++ src/client/java/fr/madu59/fwa/rendering/RenderHelper.java
@@ -26,6 +26,12 @@ public class RenderHelper {
     private static float ZShade = 0;
     private static float XShade = 0;
     private static Vector3f normal = new Vector3f();
+    private static boolean shouldShade = true;
+    private static final Direction[] DIRECTIONS_WITH_NULL = {
+        null, Direction.DOWN, Direction.UP, Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST
+    };
+    private static final Direction[] DIRECTIONS = Direction.values();
+    private static final QuadInstance quadInstance = new QuadInstance();
 
     public static void prepareFrame(AnimationRenderingContext context){
         if(!context.isShadow()){
@@ -35,6 +41,7 @@ public class RenderHelper {
             ZShade = cardinalLighting.byFace(Direction.NORTH);
             XShade = cardinalLighting.byFace(Direction.EAST);
         }
+        shouldShade = context.isShadow();
         bufferSource = context.getBufferSource();
     }
 
@@ -47,10 +54,9 @@ public class RenderHelper {
     }
 
     public static void renderModel(VertexConsumer buffer, Pose pose, List<BlockStateModelPart> parts, float a, float r, float g, float b, int light){
-        for (BlockStateModelPart part : parts){
-            renderQuads(buffer, pose, part.getQuads(null), a, r, g, b, light);
-            for(Direction dir : Direction.values()){
-                renderQuads(buffer, pose, part.getQuads(dir), a, r, g, b, light);
+        for (int i = 0; i < parts.size(); i++){
+            for (int j = 0; j < DIRECTIONS_WITH_NULL.length; j++){
+                renderQuads(buffer, pose, parts.get(i).getQuads(DIRECTIONS_WITH_NULL[j]), a, r, g, b, light);
             }
         }
     }
@@ -60,20 +66,20 @@ public class RenderHelper {
     //     Vec3 dirV = camPos.subtract(pos.getCenter());
     //     for (BlockStateModelPart part : parts){
     //         renderQuads(buffer, pose, part.getQuads(null), a, r, g, b, light);
-    //         for(Direction dir : Direction.values()){
+    //         for(Direction dir : DIRECTIONS){
     //             if(dirV.dot(dir.getUnitVec3()) >= 0) renderQuads(buffer, pose, part.getQuads(dir), a, r, g, b, light);
     //         }
     //     }
     // }
 
     public static void renderQuads(VertexConsumer buffer, Pose pose, List<BakedQuad> bakedQuads, float a, float r, float g, float b, int light){
-        for (BakedQuad bakedQuad : bakedQuads){
-            renderQuad(buffer, pose, bakedQuad, a, r, g, b, light);
+        for (int i = 0; i < bakedQuads.size(); i++){
+            renderQuad(buffer, pose, bakedQuads.get(i), a, r, g, b, light, shouldShade);
         }
     }
 
     public static void renderQuad(VertexConsumer buffer, Pose pose, BakedQuad bakedQuad, float a, float r, float g, float b, int light){
-        renderQuad(buffer, pose, bakedQuad, a, r, g, b, light, true);
+        renderQuad(buffer, pose, bakedQuad, a, r, g, b, light, shouldShade);
     }
 
     public static void renderQuad(VertexConsumer buffer, Pose pose, BakedQuad bakedQuad, float a, float r, float g, float b, int light, boolean isShaded){
@@ -90,7 +96,6 @@ public class RenderHelper {
             shade =  (nx2 * XShade) + (ny2 * yShade) + (nz2 * ZShade);
         }
 
-        QuadInstance quadInstance = new QuadInstance();
         quadInstance.setLightCoords(light);
         quadInstance.setColor(ARGB.colorFromFloat(a,r*shade,g*shade,b*shade));
         buffer.putBakedQuad(pose, bakedQuad, quadInstance);
