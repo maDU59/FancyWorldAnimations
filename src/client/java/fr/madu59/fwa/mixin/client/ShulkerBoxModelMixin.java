aa54cd6991008547f2543f4957c6f931ec4e536e
maDU59_ <matthias.ducourant@gmail.com>
2026-05-20 10:47:50 +0200

    Fixed merge

--- src/client/java/fr/madu59/fwa/mixin/client/ShulkerBoxModelMixin.java
+++ src/client/java/fr/madu59/fwa/mixin/client/ShulkerBoxModelMixin.java
@@ -8,6 +8,7 @@ import fr.madu59.fwa.FancyWorldAnimationsClient;
 import fr.madu59.fwa.config.SettingsManager;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.blockentity.ShulkerBoxRenderer;
+import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 
 @Mixin(targets = "net.minecraft.client.renderer.blockentity.ShulkerBoxRenderer$ShulkerBoxModel")
 public abstract class ShulkerBoxModelMixin {
@@ -22,8 +23,10 @@ public abstract class ShulkerBoxModelMixin {
             target = "Lnet/minecraft/client/model/geom/ModelPart;setPos(FFF)V"
         )
     )
-    private void fwa$animate(ModelPart lid, float x, float y, float z, float progress) {
-        if (progress == 1.0f && SettingsManager.SHULKERBOX_STATE.getValue()) {
+    private void fwa$animate(ModelPart lid, float x, float y, float z, ShulkerBoxBlockEntity shulkerBoxBlockEntity, float f) {
+        float progress = shulkerBoxBlockEntity.getProgress(f);
+        System.out.println(progress);
+        if (progress >= 1.0f && SettingsManager.SHULKERBOX_STATE.getValue()) {
             if(timerStart == 0) timerStart = FancyWorldAnimationsClient.getPartialTick();
             timer = FancyWorldAnimationsClient.getPartialTick() - timerStart;
             lid.setPos(x, y + (float)((Math.sin(timer * 0.1) - 0.5) * 0.3 * Math.clamp(timer * 0.04, 0.0f, 1.0f)), z);
